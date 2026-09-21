c8545368f4df3314e0729d3c45c4fa081d971fa8
fmdesigndigital-studio <fmdesigndigital@gmail.com>
2026-09-21 20:29:40 +0200

    Add files via upload
    Inicijalno postavljanje PWA fajlova

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'radar-cg-v2'; // Promijeni u v3, v4 itd. kada dodaš nove kamere
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './favicon.svg'
+];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Brisanje stare verzije keša:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
